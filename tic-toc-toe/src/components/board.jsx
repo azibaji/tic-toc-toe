4197e31fdc7a4b6c3d01489f8f5e25fc227b25fd
Arezou Saremian <arezoosaremian71@gmail.com>
2022-02-26 20:05:21 +0330

    Make all components on one and complete the game.

--- tic-toc-toe/src/components/board.jsx
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-import Square from './square';
-class Board extends Component {
-    render(i) { 
-        return <Square value={i} />
-    }
-}
- 
-export default Board;
